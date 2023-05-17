0ef8f108d441a861886b7f853fd4199353861b11
atamswaroop <agam.swaroop619@gmail.com>
2023-05-17 06:49:55 +0530

    commit 2- "Multi threading added"

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,48 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("3.Multi-threaded (both at once)");
+        Scanner in= new Scanner(System.in);
         System.out.println("Main Function started");
+        System.out.println("Enter the string you want to enter");
+        String s = in.next();
         System.out.println("Enter your Choice");
         System.out.println("1.Save as txt");
         System.out.println("2.Save as csv");
-        Scanner in= new Scanner(System.in);
+        System.out.println("3. Multi-threaded (both at once)");
         int choice = in.nextInt();
-        baseinterface client = Adapter.getid(choice);
-        System.out.println("Enter the string you want to enter");
-        String s = in.next();
-        client.enterString(s);
-
+        if(choice==3)
+        {
+            Thread t1= new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    baseinterface client1 = Adapter.getid(1);
+                    try {
+                        client1.enterString(s);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            Thread t2= new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    baseinterface client1 = Adapter.getid(2);
+                    try {
+                        client1.enterString(s);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            t1.start();
+            t2.start();
+        }
+        else {
+            baseinterface client = Adapter.getid(choice);
+            client.enterString(s);
+        }
     }
 }
-
-
